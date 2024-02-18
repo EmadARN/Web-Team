be302f0a792aab9dc6bb28e2fc0f979fa6cdb0ee
Reza <rezafafaa78@gmail.com>
2024-02-18 13:49:33 +0330

    About component pushed from reza-branch

---
+++ src/components/about/main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import About_us from './widgets/About_us'
+import {Grid} from "@mui/material"
+const About_Main = () => {
+  return (
+    <Grid display="flex" justifyContent="center" alignItems="center" width='100%' >
+    <About_us/>
+    </Grid>
+  )
+}
+
+export default About_Main
