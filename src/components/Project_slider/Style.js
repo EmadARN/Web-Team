1b81df2d145e3b51633ba3d058881dbed2d0bee8
Reza <rezafafaa78@gmail.com>
2024-02-19 12:44:46 +0330

    project slider has been maded

---
+++ src/components/Project_slider/Style.js
@@ -0,0 +1,8 @@
+export const sliderslide ={
+    width:"300px",height:"220px",backgroundSize:"cover",backgroundPosition:'center'
+}
+
+
+export const imgStyle = {
+    display:"block",width:'100%'
+}
