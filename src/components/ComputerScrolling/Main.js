ddd7f5d0ae169b8d2c7b0095380766e1362a3a95
Reza <rezafafaa78@gmail.com>
2024-02-26 16:51:05 +0330

    computer scrolling has been maded

---
+++ src/components/ComputerScrolling/Main.js
@@ -0,0 +1,9 @@
+import Computer_scroll from "./widgets/Computer_scroll";
+
+const Computer_Scroll_Main = () => {
+    return (  
+        <Computer_scroll/>
+    );
+}
+ 
+export default Computer_Scroll_Main
---
+++ src/components/ComputerScrolling/Style.js
@@ -0,0 +1,41 @@
+export const CpMainGrid ={
+    position: "relative", width: "50%",
+}
+
+
+export const CpImage ={
+    position: "absolute",
+    color: "transparent",
+    width: "100%",
+    height: "auto",
+    pointerEvents: "none",
+
+    zIndex: "5",
+    top: "0px",
+    right: "0px",
+}
+
+
+export const CpInnerGrid ={
+    overflowY: "scroll !important",
+    display: "block",
+    width: "96%",
+    paddingTop: "2rem",
+    margin: "0 auto",
+    paddingBottom: "1rem",
+    height: "350px",
+    zIndex: "4",
+    position: "absolute",
+    top:"0",
+    right:"0"
+}
+
+
+export const SiteContent ={
+    color: "transparent",
+    width: "100%",
+    height: "auto",
+    translate: "none",
+    rotate: "none",
+    scale: "none",
+}
