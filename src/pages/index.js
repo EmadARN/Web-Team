3c23555306c6c9a3c86d7f1333e1a0d69dfc3a57
Reza <rezafafaa78@gmail.com>
2024-02-27 15:59:43 +0330

    loading and cursor

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,44 +8,82 @@ import Content from "@/components/spline/Content";
 import MainSpline from "@/components/spline/Spline";
 import Layout from "@/container/main";
 import { Container, Grid } from "@mui/material";
-import React from "react";
+
+import CursorPointer from "@/components/Cursor/Cursor";
+import React, { useEffect, useState } from "react";
+import Loader from "@/components/Loader/Loader";
 
 const index = () => {
-  return (
-    <Grid
-      sx={{
-        backgroundColor: "#1b1b1b",
-      }}
-    >
-      <Layout>
-        <ParallaxHeader />
-        <Container maxWidth="lg">
-          <Grid container>
-            <Grid xs={12} md={4}>
-              <Content />
-            </Grid>
-            <Grid xs={12} md={8}>
-              <MainSpline />
-            </Grid>
-          </Grid>
-        </Container>
 
-        <Grid id="aboutUs" py={5}>
-          <About_Main />
-        </Grid>
-        <Grid id="projects" py={10}>
-          <Slider_project />
-        </Grid>
-        <Grid id="skills" py={10}>
-          <MainPersonSkils />
-        </Grid>
-        <Grid id="contactus">
-          <ContactMain />
-        </Grid>
-        <BackToUp />
-      </Layout>
-    </Grid>
-  );
+const [isLoading,setIsLoading] = useState(true)
+
+  useEffect(()=>{
+    const fakeDataFetch =()=>{
+      setTimeout(()=>{
+setIsLoading(false)
+      },7000)
+    }
+    fakeDataFetch();
+  },[])
+
+  // const [x,setX]=useState(0)
+  // const [y,setY] = useState(0)
+
+  // useEffect(()=>{
+  //   const handleMouseMovement =(e) =>{
+
+  //    setX(e.clientX)
+  //    setY(e.clientY)
+  //   }
+  //   document.addEventListener('mousemove',handleMouseMovement);
+  //   return ()=>{
+  //     document.removeEventListener('mousemove',handleMouseMovement);
+  //   }
+  // },[x,y])
+
+  return isLoading ? (
+
+<Loader/>) :(
+ <Grid
+ sx={{
+   backgroundColor: "#1b1b1b",
+ }}
+>
+ <Layout>
+   <ParallaxHeader />
+   <Container maxWidth="lg">
+     <Grid container>
+       <Grid xs={12} md={4}>
+         <Content />
+       </Grid>
+        <Grid xs={12} md={8}>
+         <MainSpline />
+       </Grid> 
+     </Grid>
+   </Container>
+
+   <Grid id="aboutUs" py={5}>
+     <About_Main />
+   </Grid>
+   <Grid id="projects" py={10}>
+     <Slider_project />
+   </Grid>
+   <Grid id="skills" py={10}>
+     <MainPersonSkils />
+   </Grid>
+   <Grid id="contactus">
+     <ContactMain />
+   </Grid>
+   <BackToUp />
+
+ {/* <CursorPointer x={x} y={y}/>  */}
+ </Layout>
+</Grid>
+)
+  
+   
+   
+
 };
 
 export default index;
