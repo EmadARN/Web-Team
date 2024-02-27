ac61ca6562579743f6eb7e3fa7983a9a9350a32c
Reza <rezafafaa78@gmail.com>
2024-02-27 12:03:26 +0330

    mobile preview

---
+++ src/components/WebsitePreview/MobileScrolling/Main.js
@@ -0,0 +1,11 @@
+import Mobile_Scroll from "./widgets/Mobile_Scroll";
+ import {Grid} from "@mui/material"
+const Mobile_Scroll_Main = () => {
+    return ( 
+        <Grid width="25%">
+        <Mobile_Scroll/>
+        </Grid>
+     );
+}
+ 
+export default Mobile_Scroll_Main;
