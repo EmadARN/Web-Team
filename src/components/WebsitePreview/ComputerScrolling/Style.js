12f964fc663ac69656017a5f3b3dd131266b90d7
Reza <rezafafaa78@gmail.com>
2024-02-29 16:59:28 +0330

    cp and mobile scroll optimized

--- src/components/WebsitePreview/ComputerScrolling/Style.js
+++ src/components/WebsitePreview/ComputerScrolling/Style.js
@@ -1,41 +1,45 @@
-export const CpMainGrid ={
-    position: "relative", width: "50%",
-}
+export const CpMainGrid = {
+  position: "relative",
+  width: "50%",
+  margin: 10,
+  display: { xs: "none", md: "block" },
+};
 
+export const CpImage = {
+  position: "absolute",
+  color: "transparent",
+  width: "100%",
+  height: "auto",
+  pointerEvents: "none",
 
-export const CpImage ={
-    position: "absolute",
-    color: "transparent",
-    width: "100%",
-    height: "auto",
-    pointerEvents: "none",
+  zIndex: "5",
+  top: "0px",
+  right: "0px",
+};
 
-    zIndex: "5",
-    top: "0px",
-    right: "0px",
-}
+export const CpInnerGrid = {
+  overflowY: "scroll !important",
+  "::-webkit-scrollbar": {
+    width: "0px",
+    height: "0px",
+  },
+  display: "block",
+  width: "90%",
+  paddingTop: "2rem",
+  margin: "24px auto",
+  paddingBottom: "1rem",
+  height: "320px",
+  zIndex: "6",
+  position: "absolute",
+  top: "40%",
+  right: "5%",
+};
 
-
-export const CpInnerGrid ={
-    overflowY: "scroll !important",
-    display: "block",
-    width: "96%",
-    paddingTop: "2rem",
-    margin: "0 auto",
-    paddingBottom: "1rem",
-    height: "350px",
-    zIndex: "4",
-    position: "absolute",
-    top:"0",
-    right:"0"
-}
-
-
-export const SiteContent ={
-    color: "transparent",
-    width: "100%",
-    height: "auto",
-    translate: "none",
-    rotate: "none",
-    scale: "none",
-}
+export const SiteContent = {
+  color: "transparent",
+  width: "100%",
+  height: "auto",
+  translate: "none",
+  rotate: "none",
+  scale: "none",
+};
