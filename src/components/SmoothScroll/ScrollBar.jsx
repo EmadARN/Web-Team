f1908a15a27c23a286baf04bdd04c9661b4e233e
Reza <rezafafaa78@gmail.com>
2024-02-29 16:58:44 +0330

    scroll smooth created

---
+++ src/components/SmoothScroll/ScrollBar.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+import Scrollbar from 'smooth-scrollbar'
+
+var options ={
+    damping :0.07
+}
+
+const ScrollBar = () => {
+    useEffect(()=>{
+Scrollbar.init(document.body,options)
+
+    },[])
+  return null
+}
+
+export default ScrollBar
