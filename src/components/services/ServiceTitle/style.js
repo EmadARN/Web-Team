605b10a4e8a7adee5494d9482210a925d475c8f6
Reza <rezafafaa78@gmail.com>
2024-03-01 19:31:56 +0330

    services has been maded

---
+++ src/components/services/ServiceTitle/style.js
@@ -0,0 +1,20 @@
+export const FirstBox ={
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-evenly",
+    "@media (max-width:912px)": {
+      display: "none",
+    },
+    m:2
+
+   
+}
+
+
+export const SteperStyle ={
+    display: "flex",
+    alignItems: "start",
+    justifyContent: "center",
+   
+    width:"50%"
+}
---
+++ src/components/services/data.js
@@ -0,0 +1,64 @@
+import animationData from '../../assets/Animation - 1709306015453.json'
+import animationData2 from '../../assets/Animation - 1709306520883.json'
+import animationData3 from '../../assets/Animation - 1709306710507.json'
+import animationData4 from '../../assets/Animation - 1709308380133.json'
+import animationData5 from '../../assets/Animation - 1709307324898.json'
+export const services = [
+  {
+    id: 1,
+    title: "توسعه و برنامه نویسی",
+    desc: "برنامه نویسی ایده ی مدنظر شما با بهره گیری از بروزترین تکنولوژی های موجود",
+    img:animationData ,
+
+    content: [
+      { subcontent: "طراحی فروشگاه های آنلاین" },
+      { subcontent: "سیستم های انبارداری و مالی" },
+      { subcontent: "توسعه ی انواع نرم افزار ها و برنامه های تحت وب" },
+    ],
+  },
+
+  {
+    id: 2,
+    title: "  سئو وبسایت",
+    desc: "سئو یا همان بالا اوردن لینک وبسایت شما در موتور های جستجو تاثیر چشم گیری در دیده شدن کسب و کار شما ایفا میکند",
+    img: animationData2,
+    content: [
+      { subcontent: "بالا اوردن لینک وبسایت " },
+      { subcontent: "فراگیری جدیدترین الگوریتم های گوگل " },
+    ],
+  },
+  {
+    id: 3,
+    title: "تولید محتوا",
+    desc: "   تولید محتوا در راستای کسب و کار شما برای همسو شدن با بازار",
+    img: animationData3,
+
+    content: [
+      { subcontent: "تیم تولید محتوای حرفه ای و متخصص" },
+      { subcontent: "تولید محتوای مختص به کسب و کار شما" },
+    ],
+  },
+  {
+    id: 4,
+    title: "پشتیبانی وبسایت",
+    desc: "با توجه به توافق انجام شده در قرارداد تیم ما از محصول مربوطه تا مدت زمان تعیین شده پشتیبانی به عمل می اورذ",
+   img: animationData4,
+
+    content: [
+      { subcontent: "رفع هرگونه خطا و باگ" },
+      {
+        subcontent: "آموزش کامل استفاده و مدیریت اپلیکیشن مربوطه به کارفرما",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "مشاوره رایگان",
+    desc: "قبل از شروع کار مشاوره کامل و جامعی به کارفرما ارائه میشود تا با اطمینان کامل محصول مورد نیازش را انتخاب کند",
+   img: animationData5,
+    content: [
+      { subcontent: "ارائه تمام نکات لازم برای انتخاب محصول" },
+      { subcontent: "راهنمایی کارفرما در طول مسیر" },
+    ],
+  },
+];
