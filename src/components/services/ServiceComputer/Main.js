c0dea828f2e4753b5b5ce06c0ecb3944ee18fc14
Reza <rezafafaa78@gmail.com>
2024-03-02 16:26:43 +0330

    Services computer updated

---
+++ src/components/services/ServiceComputer/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Services_titile from './widgets/Services_titile'
+
+const ServicesMain = () => {
+  return (
+   <Services_titile/>
+  )
+}
+
+export default ServicesMain
---
+++ src/components/services/ServiceComputer/style.js
@@ -0,0 +1,18 @@
+export const FirstBox ={
+    width: "100%",
+    display: {xs:"none",md:"flex"},
+    justifyContent: "space-evenly",
+ 
+    m:2
+
+   
+}
+
+
+export const SteperStyle ={
+    display: "flex",
+    alignItems: "start",
+    justifyContent: "center",
+   
+    width:"50%"
+}
