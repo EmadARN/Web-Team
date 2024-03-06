c037d168370d19530481785e55566a14388b76db
Reza <rezafafaa78@gmail.com>
2024-03-06 19:49:10 +0330

    about component updated

--- src/components/about/Main.jsx
+++ src/components/about/Main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import About_us from "./widgets/About_us";
-import { Grid } from "@mui/material";
+import { Grid ,Box,Typography} from "@mui/material";
 const About_Main = () => {
   return (
     <Grid
       display="flex"
       justifyContent="center"
       alignItems="center"
+      flexDirection="column"
       width="100%"
     >
+      <Box mb={6}><Typography variant="h4" fontWeight="bold">درباره ی ما</Typography></Box>
       <About_us />
     </Grid>
   );
