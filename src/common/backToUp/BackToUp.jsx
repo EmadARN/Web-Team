6f02b2a668a85929c62467ea4809c14d6c7e3107
emad <arabanemad@gmail.com>
2024-04-08 12:29:51 +0330

    optimize backToUp

--- src/common/backToUp/BackToUp.jsx
+++ src/common/backToUp/BackToUp.jsx
@@ -1,18 +1,17 @@
 import backToUp from "@/utils/BackToUp";
-import { AiOutlineArrowUp } from "react-icons/ai";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import React, { useContext } from "react";
 import { useTranslation } from "react-i18next";
 import { Btn, BtnStyle } from "./style";
 import { ThemeContext } from "@/context/ThemeContext";
 
 const BackToUp = () => {
-
-  const {t} = useTranslation()
+  const { t } = useTranslation();
   const { theme } = useContext(ThemeContext);
   return (
     <div>
       <Btn sx={BtnStyle(theme)} onClick={() => backToUp()}>
-        <AiOutlineArrowUp /> {t('backtotop')}
+        <ArrowUpwardIcon fontSize="12px" /> {t("backtotop")}
       </Btn>
     </div>
   );
