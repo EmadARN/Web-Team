b28471a3287d4fcd60702c919c0fdd946fa33396
emad <arabanemad@gmail.com>
2024-04-19 20:47:20 +0330

    gsap test 3

--- src/components/about/widgets/About_us.jsx
+++ src/components/about/widgets/About_us.jsx
@@ -16,10 +16,10 @@ const About_us = () => {
       scrollTrigger: {
         trigger: Right.current,
         trigger: Left.current,
-        start: window.innerWidth < 768 ? "500 center" : "200 center",
+        start: window.innerWidth < 768 ? "500 center" : "-30% center",
         end: "bottom center",
         scrub: false,
-        //markers: true, // برای نمایش نشانگرهای بصری
+        markers: true, // برای نمایش نشانگرهای بصری
         onEnter: () => {
           tl.play(); // شروع انیمیشن
         },
