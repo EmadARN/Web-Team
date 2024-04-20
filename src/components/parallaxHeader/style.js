ab734889793f7a05220abef12147ee5b7fbadbe2
emad <arabanemad@gmail.com>
2024-04-20 17:15:02 +0330

    Update size boat parallaxHeader

--- src/components/parallaxHeader/style.js
+++ src/components/parallaxHeader/style.js
@@ -175,9 +175,9 @@ export const boatStyle = (
 ) => {
   const style = {
     width: ExtraSmall
-      ? "40%"
+      ? "46%"
       : Small
-      ? "40%"
+      ? "44%"
       : Medium
       ? "50%"
       : Large
