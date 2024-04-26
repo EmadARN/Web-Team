9db6f6cb223c115481ba4f5da5e0dfe7ef38774a
emad <arabanemad@gmail.com>
2024-04-26 22:29:32 +0330

    Update I18next

--- src/components/contact-us/widgets/contact-mobile/Left_side_mobile.jsx
+++ src/components/contact-us/widgets/contact-mobile/Left_side_mobile.jsx
@@ -6,8 +6,7 @@ import FmdGoodIcon from "@mui/icons-material/FmdGood";
 import { T1, iconStyle } from "../../Style";
 import { useTranslation } from "react-i18next";
 const Left_side_mobile = () => {
-
-const{t} = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <Grid
@@ -41,7 +40,7 @@ const{t} = useTranslation()
           }}
         >
           <Typography sx={T1}>
-            0919111111
+            09126415284
             <LocalPhoneIcon sx={iconStyle} />
           </Typography>
         </Grid>
@@ -71,47 +70,6 @@ const{t} = useTranslation()
             <FmdGoodIcon sx={iconStyle} />
           </Typography>
         </Grid>
-        {/* <Grid
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          width="10%"
-          sx={{ pl: { xs: 6, sm: 0 } }}
-        >
-          <Typography sx={T1}>0919111111</Typography>
-
-          <LocalPhoneIcon sx={iconStyle} />
-        </Grid>
-
-        <Grid
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          width="20%"
-          sx={{ pr: { xs: 3, sm: 0 } }}
-        >
-        
-            <Typography sx={T1}>adlikara659@gmail.com</Typography>
-         
-          
-            <EmailIcon sx={iconStyle} />
-         
-        </Grid>
-
-        <Grid
-          display="flex"
-          alignItems="center"
-          justifyContent="space-between"
-          width="70%"
-          sx={{ pr: { xs: 4, sm: 0 } }}
-        >
-       
-            <Typography sx={T1}>زنجان.دانشگاه اعتمادیه</Typography>
-     
-      
-            <FmdGoodIcon sx={iconStyle} />
-    
-        </Grid> */}
       </Grid>
     </Grid>
   );
